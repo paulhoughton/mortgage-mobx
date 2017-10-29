23554d3cb86a6e7015da55f434d2548f3fdbe4c9
paulhoughton <paulhoughton@users.noreply.github.com>
2017-10-29 09:21:53 +0000

    mobx-state-tree

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
 
 import App from './App';
-import Store from './store';
-const store = new Store();
+import MortgageStore from './store';
 
-useStrict(true);
+const store = MortgageStore.create({
+  initial: 200000,
+  rate: 5,
+  years: 25,
+  monthlyOverpayment: 0,
+  overpayments: [{ year: 0, month: 1, amount: 0 }]
+});
 
 render(
   <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -1,59 +1,96 @@
-import { observable, computed, action } from 'mobx';
+import { types } from 'mobx-state-tree';
 
-export default class {
-  @observable initial = 200000;
-  @observable rate = 5;
-  @observable years = 25;
-  @observable monthlyOverpayment = 0;
-  @observable overpayments = [{ year: 0, month: 1, amount: 0 }];
+const overpayment = types.model({
+  year: types.number,
+  month: types.number,
+  amount: types.number
+});
 
-  @action setYears = (val) => this.years = val;
-  @action setRate = (val) => this.rate = val;
-  @action setInitial = (val) => this.initial = val;
-  @action setMonthlyOverpayment = (val) => this.monthlyOverpayment = val;
-  @action addOverpayment = () => this.overpayments.push({ year: 0, month: 1, amount: 0 });
-  @action removeOverpayment = (index) => this.overpayments.splice(index, 1);
-  @action setOverpayment = (field, index, val) => this.overpayments[index][field] = val;
+export default types
+  .model('Base', {
+    initial: types.number,
+    rate: types.number,
+    years: types.number,
+    monthlyOverpayment: types.number,
+    overpayments: types.array(overpayment)
+  })
+  .views(self => ({
+    get monthlyPayment() {
+      return (
+        +self.initial *
+        (self.rate / 1200) /
+        (1 - Math.pow(1 / (1 + self.rate / 1200), self.years * 12))
+      );
+    },
+    get monthlyPaymentTotal() {
+      return (self.monthlyPayment + +self.monthlyOverpayment).toFixed(2);
+    },
+    get payments() {
+      const monthlyRatePct = self.rate / 1200;
 
-  @computed get monthlyPayment() {
-    return +this.initial * (this.rate / 1200) / (1 - Math.pow(1 / (1 + this.rate / 1200), this.years * 12));
-  }
+      let balance = +self.initial;
+      let baseline = +self.initial;
+      let payments = [{ overpayment: 0, balance, baseline }];
+      let partial;
 
-  @computed get monthlyPaymentTotal() {
-    return (this.monthlyPayment + (+this.monthlyOverpayment)).toFixed(2);
-  }
+      for (let year = 0; year < self.years; year++) {
+        let interestYearly = 0;
+        let overpaymentYearly = 0;
+        for (let month = 1; month <= 12; month++) {
+          const overpayment = self.overpayments
+            .filter(x => x.year == year && x.month == month)
+            .reduce((acc, val) => acc + +val.amount, 0);
+          const interestMonth = balance * monthlyRatePct;
+          interestYearly += interestMonth;
+          overpaymentYearly += overpayment;
+          balance -=
+            self.monthlyPayment +
+            +self.monthlyOverpayment +
+            overpayment -
+            interestMonth;
+          baseline -= self.monthlyPayment - baseline * monthlyRatePct;
 
-  @computed get payments() {
-    const monthlyRatePct = this.rate / 1200;
-
-    let balance = +this.initial;
-    let baseline = +this.initial;
-    let payments = [{ overpayment: 0, balance, baseline }];
-    let partial;
-
-    for (let year = 0; year < this.years; year++) {
-      let interestYearly = 0;
-      let overpaymentYearly = 0;
-      for (let month = 1; month <= 12; month++) {
-        const overpayment = this.overpayments.filter(x => (x.year == year && x.month == month))
-          .reduce((acc, val) => acc + (+val.amount), 0);
-        const interestMonth = balance * monthlyRatePct;
-        interestYearly += interestMonth;
-        overpaymentYearly += overpayment;
-        balance -= this.monthlyPayment + (+this.monthlyOverpayment) + overpayment - interestMonth;
-        baseline -= this.monthlyPayment - (baseline * monthlyRatePct);
-
-        if (balance <= 0) {
-          balance = 0;
-          if (partial === undefined && month !== 12) {
-            partial = month;
+          if (balance <= 0) {
+            balance = 0;
+            if (partial === undefined && month !== 12) {
+              partial = month;
+            }
           }
         }
-      }
 
-      payments.push({ baseline, interestYearly, balance, partial, overpayment: overpaymentYearly + (this.monthlyOverpayment * (partial || 12)) });
-      if (partial) partial = 0;
+        payments.push({
+          baseline,
+          interestYearly,
+          balance,
+          partial,
+          overpayment:
+            overpaymentYearly + self.monthlyOverpayment * (partial || 12)
+        });
+        if (partial) partial = 0;
+      }
+      return payments;
     }
-    return payments;
-  }
-}
+  }))
+  .actions(self => ({
+    setInitial(val) {
+      self.initial = +val;
+    },
+    setRate(val) {
+      self.rate = +val;
+    },
+    setYears(val) {
+      self.years = +val;
+    },
+    setMonthlyOverpayment(val) {
+      self.monthlyOverpayment = +val;
+    },
+    addOverpayment() {
+      self.overpayments.push({ year: 0, month: 1, amount: 0 });
+    },
+    removeOverpayment(index) {
+      self.overpayments.splice(index, 1);
+    },
+    setOverpayment(field, index, val) {
+      self.overpayments[index][field] = +val;
+    }
+  }));
